c329520236a9d7b5d8bc0eb991cb4808744c2b6f
Tomasz Czarzbon <tomques11@gmail.com>
2022-12-11 04:34:25 +0100

    Before SPA

--- KontaktyBackend/KontaktyFrontend/Controllers/HomeController.cs
+++ KontaktyBackend/KontaktyFrontend/Controllers/HomeController.cs
@@ -1,27 +1,43 @@
 ﻿using KontaktyFrontend.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace KontaktyFrontend.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        Uri baseAddress = new Uri("https://localhost:7188/api");
+        HttpClient client;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
         }
 
-        public IActionResult Index()
+        public ActionResult Index()
+        {
+            List<KontaktViewModel> list = new List<KontaktViewModel>();
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress + "/Kontakt").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                list = JsonConvert.DeserializeObject<List<KontaktViewModel>>(data);
+            }
+            return View(list);
+        }
+
+        public ActionResult Details()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ KontaktyBackend/KontaktyFrontend/Models/KontaktViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace KontaktyFrontend.Models
+{
+    public class KontaktViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Imie { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Nazwisko { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [PasswordPropertyText]
+        public string Haslo { get; set; }
+
+
+        public string Kategoria { get; set; }
+
+        public string Podkategoria { get; set; }
+
+        [Phone]
+        public string Telefon { get; set; }
+
+        public DateTime Dataurodzenia { get; set; }
+    }
+}
